c3a3909e52cbef9e4165a26d437c6251794393ea
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-08 12:10:54 -0400

    remove test

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import com.sun.tools.doclets.internal.toolkit.util.links.LinkInfo;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,28 +12,55 @@ import java.util.SimpleTimeZone;
  */
 public class SinglyLinkedListTest {
 
-   @Test
-    public void getSize() {
-    SinglyLinkedList sing = new SinglyLinkedList();
-    System.out.println(sing.getSize());
-    sing.add(14);
-    sing.add(35);
-    sing.add(41);
-    System.out.println(sing.getSize());
-    Assert.assertEquals(3, sing.getSize());
-    }
+ @Test
+ public void getSize() {
+  SinglyLinkedList sing = new SinglyLinkedList();
+  System.out.println(sing.getSize());
+  sing.add(14);
+  sing.add(35);
+  sing.add(41);
+  System.out.println(sing.getSize());
+  Assert.assertEquals(3, sing.getSize());
+ }
 
-    @Test
-    public void add() {
-    SinglyLinkedList sing = new SinglyLinkedList();
-     sing.add(14);
-     sing.add(35);
-     sing.add(41);
-     sing.add(14);
-     sing.add(35);
-     sing.add(41);
-     int expected = 6;
-     int actual = sing.getSize();
-     Assert.assertEquals(expected, actual);
-    }
+ @Test
+ public void add() {
+  SinglyLinkedList sing = new SinglyLinkedList();
+  sing.add(14);
+  sing.add(35);
+  sing.add(41);
+  sing.add(14);
+  sing.add(35);
+  sing.add(41);
+  int expected = 6;
+  int actual = sing.getSize();
+  Assert.assertEquals(expected, actual);
+ }
+
+ @Test
+ public void find() {
+  SinglyLinkedList sing = new SinglyLinkedList();
+  sing.add(14);
+  sing.add(35);
+  sing.add(41);
+  sing.add(18);
+  int expected = 18;
+  int actual = sing.find(18).getData();
+  Assert.assertEquals(expected, actual);
+ }
+
+ @Test
+ public void remove() {
+  SinglyLinkedList sing = new SinglyLinkedList();
+  System.out.println(sing.getSize());
+  sing.add(4);
+  System.out.println(sing.getSize());
+  sing.add(23);
+  sing.add(18);
+  System.out.println(sing.getSize());
+  System.out.println(sing.find(23).getData());
+  sing.remove(23);
+  System.out.println(sing.find(23));
+  System.out.println(sing.getSize());
+ }
 }
